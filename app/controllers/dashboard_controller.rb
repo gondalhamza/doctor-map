be49224c6eb6ca4215409d839dacf505ca691ef9
Hamza Zubair <gondal.humza@gmail.com>
2020-10-25 22:04:05 +0100

    Map

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,23 @@
 class DashboardController < ApplicationController
+	before_action :set_doctor, only: [:index]
+	before_action :markers_hash, only: [:index]
+
   def index
+  	@bookings=@doctor.bookings
+
+  	respond_to do |format|
+      format.html
+      format.json {render json: @users}
+    end
+  end
+
+  private
+  def set_doctor
   	@doctor=Doctor.first
+  end
+
+  def markers_hash
   	@users=User.where(doctor: @doctor)
-  	@bookings=Booking.where(doctor: @doctor)
+    { page: 1, count: 200 }
   end
 end
