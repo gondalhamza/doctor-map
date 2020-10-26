45504a916db7d02a3fc2ca43fc2fd1394e5ac9d7
Hamza Zubair <gondal.humza@gmail.com>
2020-10-26 18:31:04 +0100

    Indentation fixes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,36 +1,38 @@
 require 'services/booking_service'
 
 class DashboardController < ApplicationController
-	include BookingService
+  include BookingService
 
-	before_action :set_doctor, only: [:index]
+  before_action :set_doctor, only: [:index]
 
-	def index
-		@bookings = BookingService.arrange(@doctor.bookings)
+  def index
+    @bookings = BookingService.arrange(@doctor.bookings)
 
-		ActionCable.server.broadcast(
-			'bookings',
-			bookings: @bookings
-		)
+    ActionCable.server.broadcast(
+        'bookings',
+        bookings: @bookings
+    )
 
-		respond_to do |format|
-			format.html
-			format.json {render json: @bookings}
-		end
-	end
+    respond_to do |format|
+      format.html
+      format.json { render json: @bookings }
+    end
+  end
 
-	def time_request
-	# Change time for a booking let say first booking of doctor
-		SmsJob.set(wait: 1.seconds).perform_later(Doctor.first.bookings.first)
+  def time_request
+    # Change time for a booking let say first booking of doctor
+    SmsJob.set(wait: 1.seconds).perform_later(Doctor.first.bookings.first)
 
-		render nothing: true
-	end
+    render nothing: true
+  end
 
-	private
-	def set_doctor
- 		@doctor=Doctor.first
-	end  
-	def set_booking
-		@booking=Booking.find_by_id parmas[:id]
-	end
+  private
+
+  def set_doctor
+    @doctor = Doctor.first
+  end
+
+  def set_booking
+    @booking = Booking.find_by_id parmas[:id]
+  end
 end
